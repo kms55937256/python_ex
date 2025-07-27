af48507ad0c88829e4b82eafdae7b8477fdae6a7
kms55937256 <kms55937256@gmail.com>
2025-07-27 14:41:37 +0900

    연습2

---
+++ 연습2/character_counter.py
@@ -0,0 +1,4 @@
+#2.5
+text = input("문자열을 입력하세요: ")
+char = input("찾을 문자를 입력하세요: ")
+print(f"문자 '{char}'이 {text.count(char)}번 나타납니다.")
---
+++ 연습2/word_counter.py
@@ -0,0 +1,4 @@
+# 2.4
+word = input("문장을 입력하세요: ")
+print(f"공백 제거: {' '.join(word.split())}")
+print(f"단어 개수: {len(word.split())}개")
